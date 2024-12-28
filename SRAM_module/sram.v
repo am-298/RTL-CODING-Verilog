b013d013a02364c5fe3a56d38b1b00770d610cdc
Ayushi Maurya <106837615+am-298@users.noreply.github.com>
2024-12-28 22:03:36 +0530

    Update sram.v

--- SRAM_module/sram.v
+++ SRAM_module/sram.v
@@ -1,1 +1,21 @@
-
+module simple_sram (
+    input clk,//clock signal
+    input wire wr_en,/// enable 1 - write and  0 - read
+  input [2:0] addr, // 3 - bit memory address 
+  input wire [7:0] din,       // din for write
+  output reg [7:0]dout		//dout for read
+);
+  reg [7:0] mem[0:7];
+  
+  always @ (posedge clk)
+    begin 
+      if  (!wr_en)
+        begin 
+          dout <= mem[addr];
+        end
+      else
+        begin 
+          mem[addr] <= din;
+        end
+    end
+endmodule
